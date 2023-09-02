675fe72d9c20689d89650c7443d7e89e56ad407c
IrtzaShahan <100028846+IrtzaShahan@users.noreply.github.com>
2023-09-02 21:45:54 +0500

    Update duplication_testing.py

--- duplication_testing.py
+++ duplication_testing.py
@@ -5,47 +5,44 @@ import re
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackContext
 
-
-openai.api_key = "OPENAI API KEY HERE"
-model_id = 'gpt-4'
-
+# Initialize the OpenAI API client with your API key
+openai.api_key = "YOUR_OPENAI_API_KEY"
+model_id = "gpt-3.5-turbo"
 
 # Initialize the logger
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # In-memory storage for user conversations
 user_conversations = {}
 
 
-
-## ChatGPT4 Chat Assistant Functions
 async def ai(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
     user_id = update.message.from_user.id
     nftstring = update.message.text
 
-    substring = re.sub(r'^\W*\w+\W*', '', nftstring)
+    substring = re.sub(r"^\W*\w+\W*", "", nftstring)
     print(substring)
 
-        # Append the user's message to the conversation
+    # Append the user's message to the conversation
     if user_id not in user_conversations:
         user_conversations[user_id] = []
     user_conversations[user_id].append(f"User: {substring}")
 
     system_intel = "You are the Bot, a product of the team and here to answer questions as needed."
-    #prompt = substring
-    prompt = '\n'.join(user_conversations[user_id])
+    prompt = "\n".join(user_conversations[user_id])
 
+    # Use GPT-3.5-turbo for chat completion
     response = openai.ChatCompletion.create(
-    model = model_id,
-    messages=[{"role": "system", "content": system_intel},
-              {"role": "user", "content": prompt}]
+        model=model_id,
+        messages=[
+            {"role": "system", "content": system_intel},
+            {"role": "user", "content": prompt},
+        ],
     )
-    #response_text = response["choices"][0]["message"]["content"]
 
     bot_response = response["choices"][0]["message"]["content"]
-    user_conversations[user_id].append(f"{bot_response}")
+    user_conversations[user_id].append(f"Bot: {bot_response}")
 
     await update.message.reply_text(bot_response)
 
@@ -54,8 +51,7 @@ def error(update: Update, context: CallbackContext) -> None:
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-app = ApplicationBuilder().token("BOT TOKEN HERE").build()
+app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()
 app.add_handler(CommandHandler("ai", ai))
-
 
 app.run_polling()
