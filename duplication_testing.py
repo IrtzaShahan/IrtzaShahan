fa2751959f4a11e1e1452350b48838330337591a
IrtzaShahan <100028846+IrtzaShahan@users.noreply.github.com>
2023-10-07 18:53:18 +0500

    Update duplication_testing.py

--- duplication_testing.py
+++ duplication_testing.py
@@ -1,14 +1,15 @@
 async def get_clients_groups_dict():
+    #don't change anything in below code
     try:
         clients_groups_dict = {}
         for i,phone_number in enumerate(phone_number_list):
             print(f"{phone_number} logging in and getting groups")
-            client = TelegramClient(f'my_session_{i+10}', int(api_id), api_hash)
+            client = TelegramClient(phone_number, int(api_id), api_hash)
             await client.start(phone_number)
             groups = await client.get_dialogs()
             groups = [g for g in groups if ((g.is_group) or (g.is_channel))]
-            clients_groups_dict[client] = [groups,list_of_messages[i],f'client_{i+10}',list_of_images[i]]
-            print(f" Successfully Logged in and Got list of groups for account session{i+1} ({phone_number})")
+            clients_groups_dict[client] = [groups,list_of_messages[i],f'{phone_number}']
+            print(f" Successfully Logged in and Got list of groups for account {phone_number}")
             await client.disconnect()
         return clients_groups_dict
     except Exception as e:
@@ -19,17 +20,13 @@ async def send_messages(client,groups_messages):
     for g in groups_messages[0]:
         try:
             message = groups_messages[1]
-            image = groups_messages[3]
-            await client.send_file(g,image, caption=message)
+            await client.send_message(g, message)
         except Exception as e:
-            print(f"error for group:{g.id}, {groups_messages[2]}:{e}")
+            print(f"error: {groups_messages[2]}:{e}")
         else:
             print(f"success:{groups_messages[2]}")
         sleep(interval_between_messages)
     await client.disconnect()
-
-
-
 
 async def main():
     clients_groups_dict = await get_clients_groups_dict()
@@ -40,7 +37,9 @@ async def main():
                 sleep(interval_between_accounts)
         except Exception as e:
             print(f"main error: {e}")
+
         sleep(interval_between_cycles_in_minutes*60)
 
 
 asyncio.run(main())
+
