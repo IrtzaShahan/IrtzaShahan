507904b72ea71c93b07225ada22da6decc5d39c0
IrtzaShahan <100028846+IrtzaShahan@users.noreply.github.com>
2024-02-01 22:56:20 +0500

    Update duplication_testing.py

--- duplication_testing.py
+++ duplication_testing.py
@@ -1,23 +1,28 @@
 from telethon.sync import TelegramClient,events
 from datetime import datetime, timedelta
 from time import time
-import os, requests, json
-
+import os, requests,json
+import openai
 
 api_id = 27626586
 api_hash = '90c8ff00f20929899e1cc2f16d63ffe1'
-api_key = 'f083e7e2-925e-4fe1-8bfc-1dfc48ca5625'
+api_key = '9d693983-8741-6861-6eb9-5b6911c4d4de'
+api_key ="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"
 
-langs = {'2112386913':'zh-Hant_TW','2134638726':'en_US','2122503314':'el_GR',"2068375320":"es_ES",'2124467339':'tr_TR'}
-flags = {'2112386913':'[🇨🇳](https://t.me/+6GCnV6z0kX5hMjFi)','2134638726':'[🇺🇸](https://t.me/chibaneko)','2122503314':'[🇬🇷](https://t.me/+vKqEstlNOBo5Mjgy)',"2068375320":'[🇪🇸](https://t.me/chibanekospain)',"2124467339":'[🇹🇷](https://t.me/+LqA2ir1fybo3OGMy)'}
+english = "2077495168"
+pidgin = "2003391866"
 
-channels_list = ["-1002134638726","-1002112386913","-1002122503314","-1002068375320","-1002124467339"]
+langs = {f'{english}':'en_US',f'{pidgin}':'el_GR'}
+flags = {f'{english}':'🇺🇸',f'{pidgin}':'🇬🇷'}
+
+channels_list = [f"-100{english}",f"-100{pidgin}"]
 
 client = TelegramClient('bot', api_id, api_hash)
 
-client.start()
+client.start(bot_token="6633748725:AAGIhDfz-dQ3UDSgM3u_qJ2Q_kiYScspHCE")
 
-def translate_text(text, target_language,a,b):
+
+def translate_text(text, target_language, api_key,source_language):
     url = "https://api-b2b.backenster.com/b1/api/v3/translate"
     headers = {
         "accept": "application/json",
@@ -32,13 +37,14 @@ def translate_text(text, target_language,a,b):
     }
 
     response = requests.post(url, headers=headers, json=data)
-
+    
     if 'result' in response.json():
         translated_text = response.json()['result']
         return translated_text
     else:
         print(response.json())
         return False
+
 
 def get_entities():
     entity_list= []
@@ -55,14 +61,16 @@ entities = get_entities()
 
 @client.on(events.ChatAction(entities))
 async def pin_msg_handler(message):
-    if message.action_message and message.action_message.reply_to:
-        with open('table.json','r') as fp:
-            data = json.load(fp)
-        n_l = channels_list[:]
-        n_l.remove(f"-100{message.action_message.peer_id.channel_id}")
-        for out in n_l:
-            out_channel= await client.get_input_entity(int(out))
-            await client.pin_message(out_channel,data[f"-100{message.action_message.peer_id.channel_id};{message.action_message.reply_to.reply_to_msg_id}"][out])
+    with open('table.json','r') as fp:
+        data = json.load(fp)
+    n_l = channels_list[:]
+    n_l.remove(f"-100{message.action_message.peer_id.channel_id}")
+    for out in n_l:
+        out_channel= await client.get_input_entity(int(out))
+        await client.pin_message(out_channel,data[f"-100{message.action_message.peer_id.channel_id};{message.action_message.reply_to.reply_to_msg_id}"][out])
+        print(out,data[f"-100{message.action_message.peer_id.channel_id};{message.action_message.reply_to.reply_to_msg_id}"][out])
+
+
 
 def find_object_with_key_value(data, key, value):
     for parent_key, parent_value in data.items():
@@ -110,16 +118,16 @@ async def handler(message):
                                 reply_id= None
                 else:
                     reply_id= None
-
+    
                 out_channel= await client.get_input_entity(int(out))
                 completion = translate_text(message.text.strip(), langs[out[4:]], api_key,langs[str(message.peer_id.channel_id)])
-
+            
                 if message.media:
                     if hasattr(message.media,'webpage'):
                         if message.text[:4].lower() == 'http' and len(message.text.split())==1:
                             r = await client.send_message(out_channel,f"{flags[str(message.peer_id.channel_id)]} {sender_link}\n{message.text.strip()}",link_preview=False,reply_to =reply_id)
                             msg_val[out] = r.id
-                        else:
+                        else:                           
                             r = await client.send_message(out_channel,f"{flags[str(message.peer_id.channel_id)]} {sender_link}\n{completion.strip()}",reply_to =reply_id)
                             msg_val[out] = r.id
 
@@ -132,6 +140,37 @@ async def handler(message):
 
         except Exception as e:
             print(e)
+        else:
+            print('A msg forwarded succesfully')
+            with open('table.json','r') as fp:
+                data = json.load(fp)
+            data[msg_key] = msg_val
+            with open('table.json','w') as fp:
+                json.dump(data,fp,indent=1)
+            
+    else:
+        try:
+            for out in n_l:
+                if message.reply_to:
+                    try:
+                        reply_id = data[f"-100{message.peer_id.channel_id};{message.reply_to.reply_to_msg_id}"][out]
+                    except KeyError:
+                        key, obj = find_object_with_key_value(data,f"-100{message.peer_id.channel_id}",message.reply_to.reply_to_msg_id)
+                        if out in key:
+                            reply_id = int(key.split(';')[-1])
+                        else:
+                            try:
+                                reply_id = obj[out]
+                            except:
+                                reply_id= None
+                else:
+                    reply_id= None
+
+                out_channel= await client.get_input_entity(int(out))
+                r = await client.send_file(out_channel,message.media,caption=f"{flags[str(message.peer_id.channel_id)]} {sender_link}")
+                msg_val[out] = r.id
+        except Exception as e:
+            print(e)
             print(type(e).__name__)
         else:
             print('A msg forwarded succesfully')
@@ -140,15 +179,6 @@ async def handler(message):
             data[msg_key] = msg_val
             with open('table.json','w') as fp:
                 json.dump(data,fp,indent=1)
-
-    else:
-        try:
-            for out in n_l:
-                out_channel= await client.get_input_entity(int(out))
-                await client.send_file(out_channel,message.media,caption=f"{flags[str(message.peer_id.channel_id)]} {sender_link}")
-        except Exception as e:
-            print(e)
-            print(type(e).__name__)
 
 
 print('\nBot is started and will try to forward all rcvd signals from source groups to destination group, please make sure to leave this window open(do not close it)\n')
