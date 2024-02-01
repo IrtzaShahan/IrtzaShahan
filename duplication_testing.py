20efa48cfa28ef560a8fc5b937827b320a8b0b80
IrtzaShahan <100028846+IrtzaShahan@users.noreply.github.com>
2024-02-01 22:55:56 +0500

    Update duplication_testing.py

--- duplication_testing.py
+++ duplication_testing.py
@@ -1,45 +1,156 @@
-async def get_clients_groups_dict():
-    try:
-        clients_groups_dict = {}
-        for i,phone_number in enumerate(phone_number_list):
-            print(f"{phone_number} logging in and getting groups")
-            client = TelegramClient(phone_number, int(api_id), api_hash)
-            await client.start(phone_number)
-            groups = await client.get_dialogs()
-            groups = [g for g in groups if ((g.is_group) or (g.is_channel))]
-            clients_groups_dict[client] = [groups,list_of_messages[i],f'b2({i+1}):{phone_number}',list_of_images[i]]
-            print(f" Successfully Logged in and Got list of groups for account {phone_number}")
-            await client.disconnect()
-        return clients_groups_dict
-    except Exception as e:
-        print(f"Error occurred: {e}")
+from telethon.sync import TelegramClient,events
+from datetime import datetime, timedelta
+from time import time
+import os, requests, json
 
 
-async def send_messages(client,groups_messages):
-    await client.connect()
-    for g in groups_messages[0]:
+api_id = 27626586
+api_hash = '90c8ff00f20929899e1cc2f16d63ffe1'
+api_key = 'f083e7e2-925e-4fe1-8bfc-1dfc48ca5625'
+
+langs = {'2112386913':'zh-Hant_TW','2134638726':'en_US','2122503314':'el_GR',"2068375320":"es_ES",'2124467339':'tr_TR'}
+flags = {'2112386913':'[🇨🇳](https://t.me/+6GCnV6z0kX5hMjFi)','2134638726':'[🇺🇸](https://t.me/chibaneko)','2122503314':'[🇬🇷](https://t.me/+vKqEstlNOBo5Mjgy)',"2068375320":'[🇪🇸](https://t.me/chibanekospain)',"2124467339":'[🇹🇷](https://t.me/+LqA2ir1fybo3OGMy)'}
+
+channels_list = ["-1002134638726","-1002112386913","-1002122503314","-1002068375320","-1002124467339"]
+
+client = TelegramClient('bot', api_id, api_hash)
+
+client.start()
+
+def translate_text(text, target_language,a,b):
+    url = "https://api-b2b.backenster.com/b1/api/v3/translate"
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "a_Srrj954JR2Kp5RYULdKL3tCNy1sd5UoG6SDFlPRuAmeos7BJePiTJ5ESjSVQfEMWoo1ZPrnZdr5JlaIz"
+    }
+
+    data =  {
+        "platform": "api",
+        "to": target_language,
+        "data": text
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if 'result' in response.json():
+        translated_text = response.json()['result']
+        return translated_text
+    else:
+        print(response.json())
+        return False
+
+def get_entities():
+    entity_list= []
+    for channel in channels_list:
         try:
-            message = groups_messages[1]
-            image = groups_messages[3]
-            await client.send_file(g,image, caption=message)
+            input_par = int(channel)
+        except:
+            input_par = channel
+        entity=client.get_input_entity(input_par)
+        entity_list.append(entity)
+    return entity_list
+
+entities = get_entities()
+
+@client.on(events.ChatAction(entities))
+async def pin_msg_handler(message):
+    if message.action_message and message.action_message.reply_to:
+        with open('table.json','r') as fp:
+            data = json.load(fp)
+        n_l = channels_list[:]
+        n_l.remove(f"-100{message.action_message.peer_id.channel_id}")
+        for out in n_l:
+            out_channel= await client.get_input_entity(int(out))
+            await client.pin_message(out_channel,data[f"-100{message.action_message.peer_id.channel_id};{message.action_message.reply_to.reply_to_msg_id}"][out])
+
+def find_object_with_key_value(data, key, value):
+    for parent_key, parent_value in data.items():
+        if key in parent_value and parent_value[key] == value:
+            return parent_key, parent_value
+    return None
+
+@client.on(events.NewMessage(entities,incoming=True))
+async def handler(message):
+    n_l = channels_list[:]
+    n_l.remove(f"-100{message.peer_id.channel_id}")
+    msg_key,msg_val = f"-100{message.peer_id.channel_id};{message.id}",{}
+    with open('table.json','r') as fp:
+        data = json.load(fp)
+
+    sender = await message.get_sender()
+    sender_name = ''
+    if sender:
+        if hasattr(sender,'first_name') and sender.first_name:
+            sender_name =  sender.first_name+' '
+        if hasattr(sender,'last_name') and sender.last_name:
+            sender_name += sender.last_name
+        if (not sender_name) and hasattr(sender,'username') and sender.username:
+            sender_name = sender.username
+
+        sender_link = f"[{sender_name}](https://t.me/{sender.username})"
+    else:
+        sender_link = ''
+    if message.text:
+        if message.text.startswith('/'):
+            return
+        try:
+            for out in n_l:
+                if message.reply_to:
+                    try:
+                        reply_id = data[f"-100{message.peer_id.channel_id};{message.reply_to.reply_to_msg_id}"][out]
+                    except KeyError:
+                        key, obj = find_object_with_key_value(data,f"-100{message.peer_id.channel_id}",message.reply_to.reply_to_msg_id)
+                        if out in key:
+                            reply_id = int(key.split(';')[-1])
+                        else:
+                            try:
+                                reply_id = obj[out]
+                            except:
+                                reply_id= None
+                else:
+                    reply_id= None
+
+                out_channel= await client.get_input_entity(int(out))
+                completion = translate_text(message.text.strip(), langs[out[4:]], api_key,langs[str(message.peer_id.channel_id)])
+
+                if message.media:
+                    if hasattr(message.media,'webpage'):
+                        if message.text[:4].lower() == 'http' and len(message.text.split())==1:
+                            r = await client.send_message(out_channel,f"{flags[str(message.peer_id.channel_id)]} {sender_link}\n{message.text.strip()}",link_preview=False,reply_to =reply_id)
+                            msg_val[out] = r.id
+                        else:
+                            r = await client.send_message(out_channel,f"{flags[str(message.peer_id.channel_id)]} {sender_link}\n{completion.strip()}",reply_to =reply_id)
+                            msg_val[out] = r.id
+
+                    else:
+                        r = await client.send_file(out_channel,message.media,caption=f"{flags[str(message.peer_id.channel_id)]} {sender_link}\n{completion.strip()}",reply_to =reply_id)
+                        msg_val[out] = r.id
+                else:
+                    r = await client.send_message(out_channel,f"{flags[str(message.peer_id.channel_id)]} {sender_link}\n{completion.strip()}",link_preview=False,reply_to =reply_id)
+                    msg_val[out] = r.id
+
         except Exception as e:
-            print(f"error for group:{g.id}, {groups_messages[2]}:{e}")
+            print(e)
+            print(type(e).__name__)
         else:
-            print(f"success:{groups_messages[2]}")
-        sleep(interval_between_messages)
-    await client.disconnect()
+            print('A msg forwarded succesfully')
+            with open('table.json','r') as fp:
+                data = json.load(fp)
+            data[msg_key] = msg_val
+            with open('table.json','w') as fp:
+                json.dump(data,fp,indent=1)
 
-
-async def main():
-    clients_groups_dict = await get_clients_groups_dict()
-    while True:
+    else:
         try:
-            for client,groups_messages in clients_groups_dict.items():
-                await send_messages(client,groups_messages)
-                sleep(interval_between_accounts)
+            for out in n_l:
+                out_channel= await client.get_input_entity(int(out))
+                await client.send_file(out_channel,message.media,caption=f"{flags[str(message.peer_id.channel_id)]} {sender_link}")
         except Exception as e:
-            print(f"main error: {e}")
-        sleep(interval_between_cycles_in_minutes*60)
+            print(e)
+            print(type(e).__name__)
 
 
-asyncio.run(main())
+print('\nBot is started and will try to forward all rcvd signals from source groups to destination group, please make sure to leave this window open(do not close it)\n')
+
+client.run_until_disconnected()
